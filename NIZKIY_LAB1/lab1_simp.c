912e1e6b22803cd41541de4e37cf800b38cfa438
vartanchik12 <147078735+vartanchik12@users.noreply.github.com>
2024-11-20 22:45:47 +0400

    Update lab1_simp.c

--- NIZKIY_LAB1/lab1_simp.c
+++ NIZKIY_LAB1/lab1_simp.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    char input[256];       // Буфер для строки
-    int length = 0;        // Длина строки
-    int is_palindrome = 1; // Флаг палиндрома
-    int start = 0, end;    // Индексы для проверки
+    char input[256]; 
+    int length = 0;
+    int is_palindrome = 1;
+    int start = 0, end;
 
-    // Ввод строки
     printf("Enter a string: ");
     fgets(input, 256, stdin);
 
-    // Определение длины строки
 find_length:
     if (input[length] != '\0' && input[length] != '\n') {
         length++;
         goto find_length;
     }
 
-    // Установка индекса конца строки
     end = length - 1;
 
 check_palindrome:
-    if (start >= end) goto result; // Условие завершения проверки
+    if (start >= end) goto result; 
 
     if (input[start] != input[end]) {
         is_palindrome = 0;
@@ -33,7 +30,6 @@ check_palindrome:
     goto check_palindrome;
 
 result:
-    // Вывод результата
     if (is_palindrome) {
         printf("The string is a palindrome\n");
         goto end_program;
