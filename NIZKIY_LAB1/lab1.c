b665d58b1964f81b62d6fdd8d12a16ac38977542
vartanchik12 <147078735+vartanchik12@users.noreply.github.com>
2024-11-20 22:45:11 +0400

    Update lab1.c

--- NIZKIY_LAB1/lab1.c
+++ NIZKIY_LAB1/lab1.c
@@ -7,20 +7,16 @@ int main() {
     int length;
     bool is_palindrome = true;
 
-    // Ввод строки
     printf("Enter a string: ");
     fgets(input, sizeof(input), stdin);
 
-    // Удаление символа новой строки, если он есть
     size_t len = strlen(input);
     if (input[len - 1] == '\n') {
         input[len - 1] = '\0';
     }
 
-    // Определение длины строки
     length = strlen(input);
 
-    // Проверка строки на палиндром
     for (int i = 0, j = length - 1; i < j; i++, j--) {
         if (input[i] != input[j]) {
             is_palindrome = false;
@@ -28,7 +24,6 @@ int main() {
         }
     }
 
-    // Вывод результата
     if (is_palindrome) {
         printf("%s\n", input);
         printf("The string is a palindrome\n");
